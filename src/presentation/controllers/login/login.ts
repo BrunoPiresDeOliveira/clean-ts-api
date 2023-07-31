ff877ed0272535586a98371f461e760e15327d96
Bruno Pires Xavier <bruno.oliveira@squidit.com.br>
2023-07-31 00:11:01 -0300

    test: ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -5,8 +5,15 @@ import type { Controller } from '../../protocols/controller'
 
 export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return await new Promise(resolve => {
-      resolve(badRequest(new MissingParamError('email')))
-    })
+    if (!httpRequest.body.email) {
+      return await new Promise(resolve => {
+        resolve(badRequest(new MissingParamError('email')))
+      })
+    }
+    if (!httpRequest.body.password) {
+      return await new Promise(resolve => {
+        resolve(badRequest(new MissingParamError('password')))
+      })
+    }
   }
 }
