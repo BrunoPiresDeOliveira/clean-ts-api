e1c3bb4384189ce074681375fffcfd0a34424058
Bruno Pires Xavier <bruno.oliveira@squidit.com.br>
2023-07-30 22:21:22 -0300

    test: ensure LogMongoRepository creates an error log on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -26,7 +26,6 @@ describe('Account Mongo Repository', () => {
       email: 'any_email@mail.com',
       password: 'any_password'
     })
-    console.log(account)
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
