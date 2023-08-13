7f29f56b7a5c238b160d5ff32daf5e51a1faacdb
Bruno Pires Xavier <bruno.oliveira@squidit.com.br>
2023-08-13 16:38:14 -0300

    refactor: organize main layer folders

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 import type { Router } from 'express'
-import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
-import { adaptRoute } from '../adapters/express/express-routes'
-import { makeLoginController } from '../factories/controllers/login/login-controller-factory'
+import { makeSignUpController } from '../factories/controllers/account/signup/signup-controller-factory'
+import { adaptRoute } from '../adapters/express-routes'
+import { makeLoginController } from '../factories/controllers/account/login/login-controller-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,6 +1,6 @@
 import type { Router } from 'express'
-import { makeAddSurveyController } from '../factories/controllers/add-survey/add-survey-controller-factory'
-import { adaptRoute } from '../adapters/express/express-routes'
+import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { adaptRoute } from '../adapters/express-routes'
 
 export default (router: Router): void => {
   router.post('/surveys', adaptRoute(makeAddSurveyController()))
