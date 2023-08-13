648019be0d3f3cbeef094a2d6a4782c4c4f2036d
Bruno Pires Xavier <bruno.oliveira@squidit.com.br>
2023-08-13 18:42:28 -0300

    feat: ensure LoadSurveysController calls LoadSurveysUseCase

---
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -0,0 +1,10 @@
+import type { Controller, HttpRequest, HttpResponse, LoadSurveys } from './load-serveys-controller-protocols'
+
+export class LoadSurveysController implements Controller {
+  constructor (private readonly loadSurveys: LoadSurveys) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveys.load()
+    return null
+  }
+}
