bf3a8a06b559c1bbcb92b5f74b288571a6e0aaa7
Bruno Pires Xavier <bruno.oliveira@squidit.com.br>
2023-08-14 23:38:51 -0300

    test: ensure SurveyResultMongoRepository updates surveyResult if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -18,7 +18,6 @@ const makeAccount = async (): Promise<AccountModel> => {
     email: 'any_email',
     password: 'any_password'
   })
-  console.log(account)
   const result = await accountCollection.findOne({ _id: account.insertedId })
   return result && MongoHelper.map(result)
 }
@@ -71,6 +70,27 @@ describe('Survey Mongo Repository', () => {
       expect(surveyResult).toBeTruthy()
       expect(surveyResult.id).toBeTruthy()
       expect(surveyResult.answer).toBe(survey.answers[0].answer)
+    })
+
+    test('Should update survey result if its not new', async () => {
+      const survey = await makeSurvey()
+      const account = await makeAccount()
+      const newSurvey = await surveyResultCollection.insertOne({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.answers[1].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id.toString()).toEqual(newSurvey.insertedId.toString())
+      expect(surveyResult.answer).toBe(survey.answers[1].answer)
     })
   })
 })
