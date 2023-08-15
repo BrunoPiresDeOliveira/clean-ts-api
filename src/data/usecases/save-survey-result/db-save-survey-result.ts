cf12dd5ccdab5c5d436ee56d6baad34878874b79
Bruno Pires Xavier <bruno.oliveira@squidit.com.br>
2023-08-14 21:40:53 -0300

    test: ensure DbSaveSurveyResult returns a SurveyResult on success

--- src/data/usecases/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -4,7 +4,7 @@ export class DbSaveSurveyResult implements SaveSurveyResult {
   constructor (private readonly saveSurveyResultRepository: SaveSurveyResultRepository) {}
 
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-    const surveys = await this.saveSurveyResultRepository.save(data)
-    return surveys
+    const surveyResult = await this.saveSurveyResultRepository.save(data)
+    return surveyResult
   }
 }
