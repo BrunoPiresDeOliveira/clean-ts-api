21a1bc6b069e28fff9e5ae2018c80c68e0b7212a
Bruno Pires Xavier <bruno.oliveira@squidit.com.br>
2023-08-23 22:06:38 -0300

    feat: ensure LoadSurveyResultController returns 200 on succes

--- src/presentation/controllers/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/load-survey-result/load-survey-result-controller.ts
@@ -1,4 +1,4 @@
-import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
+import { forbidden, ok, serverError } from '@/presentation/helpers/http/http-helper'
 import type { Controller, HttpRequest, HttpResponse, LoadSurveyById, LoadSurveyResult } from './load-survey-result-controller-protocols'
 import { InvalidParamsError } from '@/presentation/errors'
 
@@ -16,8 +16,8 @@ export class LoadSurveyResultController implements Controller {
       if (!survey) {
         return forbidden(new InvalidParamsError('surveyId'))
       }
-      await this.loadSurveyResult.load(surveyId)
-      return null
+      const surveyResult = await this.loadSurveyResult.load(surveyId)
+      return ok(surveyResult)
     } catch (error) {
       return serverError(error)
     }
