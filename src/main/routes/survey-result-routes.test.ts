fb521a8950486530ff7cc937e785943fa40c29e6
Bruno Pires Xavier <bruno.oliveira@squidit.com.br>
2023-08-23 22:25:54 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -88,5 +88,23 @@ describe('Survey Routes', () => {
         .get('/api/surveys/any_id/results')
         .expect(403)
     })
+
+    test('Should return 200 on load survey result with accessToken', async () => {
+      await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      })
+      const { _id } = await surveyCollection.findOne({ question: 'any_question' })
+      await request(app)
+        .get(`/api/surveys/${_id.toString()}/results`)
+        .set('x-access-token', await makeAccessToken())
+        .expect(200)
+    })
   })
 })
